b0d453f06c814711dc475f5345f7362ee56ff3be
luyangsuper <luyangsuper@gmail.com>
2022-04-07 20:24:38 +0800

    修改搜索默认值

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import {defineStore} from 'pinia'
 export const useMainStore = defineStore('main', {
     state: () => {
         return {
-            searchValue: '可以使用'
+            searchValue: ''
         }
     }
 })
