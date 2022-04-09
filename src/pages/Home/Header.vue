a79aa98c219966d016f5e1713f82d7fc72f840e8
Ricardo_M_Lu <55616442+5120170912@users.noreply.github.com>
2022-04-09 18:38:04 +0800

    背景跟随熊脚步

--- src/pages/Home/Header.vue
+++ src/pages/Home/Header.vue
@@ -42,7 +42,7 @@ const onSearch = () => {
     // background-color: #f6f6f6;
     background: url("@/assets/img/bear-background.png") repeat;
     background-size: 10% 100%;
-    animation: bearBgMove 10s linear infinite forwards;
+    animation: bearBgMove 20s linear infinite forwards;
     display: flex;
 
     .input-search {
@@ -56,7 +56,7 @@ const onSearch = () => {
         background: url("@/assets/img/brear.png");
         background-size: 400px 25px;
         background-repeat: no-repeat;
-        animation: bear 0.8s steps(8) infinite, move 100s linear forwards;
+        animation: bear 0.8s steps(8) infinite, move 20s linear forwards;
     }
 }
 
@@ -89,12 +89,28 @@ const onSearch = () => {
         transform: rotateY(0);
     }
 }
-@keyframes bearBgMove{
-    0%{
+@keyframes bearBgMove {
+    0% {
         background-position: 0 0;
     }
-    100%{
+    50% {
+        background-position: -400px 0; //图片往左移动
+    }
+
+    99% {
+        background-position: 0 0;
+    }
+    100% {
         background-position: -400px 0; //图片往左移动
     }
 }
+// @keyframes bearBgMove2 {
+//     0%{
+//         background-position: 0 0;
+//     }
+
+//     100%{
+//          background-position: 400px 0; //图片往右移动
+//     }
+// }
 </style>
